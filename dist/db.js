a9b162393d4fe75f18c6aef38ea9761ec9f4ec73
Денис Соколов <sokolovdenisost@gmail.com>
2021-07-25 18:14:02 +0300

    add url mysql connection

--- dist/db.js
+++ dist/db.js
@@ -1,13 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '29891969Вв',
-    database: 'service',
-    port: 3306,
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '29891969Вв',
+//     database: 'service',
+//     port: 3306,
+// });
+const connection = mysql.createConnection('mysql://root:29891969Вв@localhost/service');
 exports.connection = connection;
 connection.connect((err) => {
     if (err) {
--- server/db.ts
+++ server/db.ts
@@ -1,11 +1,13 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '29891969Вв',
-    database: 'service',
-    port: 3306,
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '29891969Вв',
+//     database: 'service',
+//     port: 3306,
+// });
+
+const connection = mysql.createConnection('mysql://root:29891969Вв@localhost/service');
 
 connection.connect((err) => {
     if (err) {
